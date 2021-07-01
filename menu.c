23728c2544f2d838feefda8c4f776d5a548fdd36
nvhwork <hoang.nv176762@sis.hust.edu.vn>
2021-07-01 21:47:14 +0700

    Code for Client

---
+++ menu.c
@@ -0,0 +1,16 @@
+#include "menu.h"
+
+void menu_home(int* figures) {
+	printf("---------------------------\n");
+	printf("--  Danh sách đồ uống:   --\n");
+	printf(	"-- 1.  Pepsi           %2d--\n"  
+			"-- 2.  Coca            %2d--\n"
+			"-- 3.  Lala            %2d--\n"
+			"-- 4.  Quit              --\n", figures[0], figures[1], figures[2]);
+	printf("---------------------------\n");
+	printf("Your choice: ");
+}
+
+void menu_bye() {
+  printf("Bye\n");
+}
---
+++ menu.h
@@ -0,0 +1,10 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <stdio.h>
+#include <string.h>
+
+void menu_home();
+void menu_bye();
+	
+#endif

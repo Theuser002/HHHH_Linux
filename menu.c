8b637c0ab0074681468226395af31f12085c5289
I001user <hung.nt176773@sis.hust.edu.vn>
2021-08-08 19:08:27 +0700

    change drink name to something which is less of a joke

--- menu.c
+++ menu.c
@@ -3,9 +3,9 @@
 void menu_home(int* figures) {
 	printf(	"---------------------------\n"	);
 	printf(	"--          Menu:        --\n"	);
-	printf(	"-- 1. Pepsi          %3d --\n"  
-			"-- 2. Coca           %3d --\n"
-			"-- 3. Lala           %3d --\n"
+	printf(	"-- 1. Coke           %3d --\n"  
+			"-- 2. Water          %3d --\n"
+			"-- 3. Milk           %3d --\n"
 			"-- 0. Quit               --\n", figures[0], figures[1], figures[2]);
 	printf(	"---------------------------\n"	);
 	printf(	"Your choice: "	);
